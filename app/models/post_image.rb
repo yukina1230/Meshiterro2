cf42be79f1d6ebecf76f1b472ef3d07fcf84d266
EC2 Default User <lost.laugh1230@gmail.com>
2021-03-17 02:55:17 +0000

    [Finish]

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,5 +1,4 @@
 class PostImage < ApplicationRecord
-
   belongs_to :user
   attachment :image
   has_many :post_comments, dependent: :destroy
@@ -11,5 +10,4 @@ class PostImage < ApplicationRecord
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
-
 end
